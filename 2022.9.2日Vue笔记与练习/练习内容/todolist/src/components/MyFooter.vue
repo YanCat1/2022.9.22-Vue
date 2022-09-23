e70f4155ecfadd65e0bf79348a95a2c2eb021dc3
yanzd <2223126487@qq.com>
2022-09-23 14:27:21 +0800

    2022.9.23日更新
    完成了组件样式和主要内容的编写，进行了注意事项的更新

--- 2022.9.2日Vue笔记与练习/练习内容/todolist/src/components/MyFooter.vue
+++ 2022.9.2日Vue笔记与练习/练习内容/todolist/src/components/MyFooter.vue
@@ -1,13 +1,49 @@
 <template>
-  
+  <div class="todo-footer">
+    <label for="">
+        <input type="checkbox">
+    </label>
+    <span>
+        <span>已完成0</span>/全部2
+    </span>
+    <button class="btn btn-danger">清除已完成</button>
+  </div>
 </template>
 
 <script>
 export default {
     name:'MyFooter'
 }
-</script>
+</script >
 
-<style>
+<style scoped>
+    .todo-footer{
+        height:40px;
+        line-height:40px;
+        padding-left:6px;
+        margin-top:5px;
+    }
+    .todo-footer label{
+        display:inline-block;
+        margin-right:20px;
+        cursor:pointer;
+    }
+    .todo-footer label input{
+        position:relative;
+        top:-1px;
+        vertical-align:middle;
+        margin-right:5px;
+    }
+    li button{
+        float:right;
+        display:none;
+        margin-top:3px;
+    }
+    li:before{
+        content:initial;
+    }
+    li:last-child{
+        border-bottom:none;
+    }
 
 </style>
